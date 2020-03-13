2d09532837c61e8fde67e4ef184fc5d249a48c8a
tacswilliams <55443380+tacswilliams@users.noreply.github.com>
2020-03-13 08:54:08 -0400

    GameManager Variables Created

---
+++ Stackers/Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameOver;
+    public bool collidedWithGround = false;
+    public int moveCount = 0;
+    public Transform crateSpawnPos;
+    public Vector3 cameraTargetPos;
+    public TextMeshProUGUI bestScore;
+    public TextMeshProUGUI scoreText;
+    public int score = 0;
+    public GameObject gameOverText;
+    //Crate crate;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
